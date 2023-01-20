90a21a1c8e70c2e1aa4dd82936c27e21ae125d11
thequlmatov <zuxroka0104@gmail.com>
2023-01-20 19:12:14 +0500

    To do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Input from './Todos/Input';
+import {datas} from './Todos/Api';
 
-function App() {
+const App = () => {
+
+  // inputni qiymatlarini olish qismi
+  const [api,setApi] = useState(datas);
+  const [name,setName] = useState("");
+  const [price,setPrice] = useState("");
+  const [year,setYear] = useState("");
+
+ // inputdan olingan qiymatlarni API ichiga qo`shib yuborish qismi
+  const addDatas = (e) => {
+    let newInfo = {
+      id:Date.now(),
+      name:name,
+      price:price,
+      year:year
+    }
+
+    setApi(  [...api , newInfo] );
+    setName('');
+    setPrice('');
+    setYear('');
+
+    e.preventDefault();
+  }
+
+  // tanlangan itemni o`chirib tashlash qismi
+
+      const removeItem = (id , e) => {
+        let newData = api.filter( oby => {
+          return oby.id !=id
+        })
+        setApi(newData);
+
+        e.preventDefault();
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='admin-panel'>
+      <form>
+      <Input value = {name} onChange={(e) => setName(e.target.value)}  className="form-control mt-3" type="text" placeholder="Product Name" />
+      <Input value = {price} onChange={(e) => setPrice(e.target.value)}  className="form-control mt-3" type="text" placeholder="Price" />
+      <Input value = {year} onChange={(e) => setYear(e.target.value)}  className="form-control mt-3" type="text" placeholder="Year" />
+      <button onClick={addDatas} className='btn btn-success mt-3 w-100'>Add to List</button>
+
+      <table className="table table-hover mt-3">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Brand Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Year</th>
+            <th scope="col">Deleting</th>
+          </tr>
+        </thead>
+
+
+        {
+          api.length == 0
+          ? <h5>List is empty, please add something</h5>
+          : <tbody>
+          {
+           api.map((arr, index) => {
+             return  <tr key ={arr.id}>
+             <th scope="row">{index + 1}</th>
+             <td>{arr.name}</td>
+             <td>{arr.price}</td>
+             <td>{arr.year}</td>
+             <td>
+               <button onClick={() => removeItem(arr.id)} className='btn btn-danger'>Remove</button>
+             </td>
+           </tr>
+           })
+          }
+         </tbody>
+        }
+
+
+      </table>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Todos/Api.js
@@ -0,0 +1,26 @@
+export let datas = [
+  {
+    id:1,
+    name:'Iphone 13 pro Max',
+    price:'$1600',
+    year:2021
+  },
+  {
+    id:2,
+    name:'Samsung s22 ultra',
+    price:'$1200',
+    year:2020
+  },
+  {
+    id:3,
+    name:'Redmi note 12 pro',
+    price:'$800',
+    year:2022
+  },
+  {
+    id:4,
+    name:'Tesla phone',
+    price:'$999',
+    year:2024
+  }
+]
